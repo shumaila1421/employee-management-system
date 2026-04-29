085005336a0feb82ac6dad35e1f94f00ac8d1f83
Shumaila Riaz <shumailariaz215@gmail.com>
2026-04-29 20:38:55 +0500

    attendance function added

--- client/src/pages/Attendance.jsx
+++ client/src/pages/Attendance.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { dummyAttendanceData } from "../assets/assets";
 
 const Attendance = () => {
+  const [history, setHistory] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const fetchData = useCallback(async () => {
+    setHistory(dummyAttendanceData);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
   return <div>Attandance</div>;
 };
 
--- client/src/pages/Employees.jsx
+++ client/src/pages/Employees.jsx
@@ -22,7 +22,7 @@ const Employees = () => {
     setTimeout(() => {
       setLoading(false);
     }, 1000);
-  }, []);
+  }, [selectDept]);
 
   useEffect(() => {
     fetchEmployees();
